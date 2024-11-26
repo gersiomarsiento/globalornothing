d1f7c77c97b09453fd524a6b2466b36d4415b46f
Sergio Sarmiento <gersiomarsiento@gmail.com>
2024-11-26 14:28:52 -0300

    test padding header

--- components/header.jsx
+++ components/header.jsx
@@ -14,7 +14,7 @@ const navItems = [
 
 export function Header() {
     return (
-        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
+        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12">
             <Link href="/">
                 <Image src={netlifyLogo} alt="Netlify logo" />
             </Link>
